58321846eb0ccf46d33c3212c8975da3c95f7601
Pathum312 <pathum.2019557@iit.ac.lk>
2024-02-13 13:54:37 +0530

    Added Category Swagger Doc

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -12,18 +12,124 @@ const categoryService = new CategoryService({
 	logger,
 });
 
-router.get('/', async (req, res, next) => {
-	await categoryService.get(req, res, next);
-});
-
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Add a category
+ *     tags: [Category]
+ *     security:
+ *       - JWTAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 default: PS4 Games
+ *               description:
+ *                 type: string
+ *                 default: Games for the PS4
+ *             required:
+ *               - name
+ *               - description
+ *     responses:
+ *       '201':
+ *         description: Created.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.post('/', async (req, res, next) => {
 	await categoryService.create(req, res, next);
 });
 
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     summary: Get categories list
+ *     tags: [Category]
+ *     security:
+ *       - JWTAuth: []
+ *     parameters: [
+ *      {
+ *          name: 'name',
+ *          in: 'query',
+ *          description: 'PS4 Games'
+ *      },
+ *     ]
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
+router.get('/', async (req, res, next) => {
+	await categoryService.get(req, res, next);
+});
+
+/**
+ * @swagger
+ * /categories:
+ *   put:
+ *     summary: Update category details
+ *     tags: [Category]
+ *     security:
+ *       - JWTAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 default: 65rrfgsfgr23w45gfsd
+ *               name:
+ *                 type: string
+ *                 default: PS4 Games
+ *               description:
+ *                 type: string
+ *                 default: Games for the PS4
+ *             required:
+ *               - id
+ *               - name
+ *               - description
+ *     responses:
+ *       '200':
+ *         description: Internal server error.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.put('/', async (req, res, next) => {
 	await categoryService.update(req, res, next);
 });
 
+/**
+ * @swagger
+ * /categories/{id}:
+ *   delete:
+ *     summary: Delete category by id
+ *     tags: [Category]
+ *     security:
+ *       - JWTAuth: []
+ *     parameters: [
+ *      {
+ *          name: 'id',
+ *          in: 'path',
+ *          description: '65dsfsfbfh5634'
+ *      },
+ *     ]
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.delete('/:id', async (req, res, next) => {
 	await categoryService.destroy(req, res, next);
 });
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -12,10 +12,40 @@ const AuthController = require('./auth.controller');
  *     description: API endpoints for user account management
  */
 const UserController = require('./user.controller');
+/**
+ * @swagger
+ * tags:
+ *   - name: Category
+ *     description: API endpoints for category management
+ */
 const CategoryController = require('./category.controller');
+/**
+ * @swagger
+ * tags:
+ *   - name: Product
+ *     description: API endpoints for product management
+ */
 const ProductController = require('./product.controller');
+/**
+ * @swagger
+ * tags:
+ *   - name: Cart
+ *     description: API endpoints for cart management
+ */
 const CartController = require('./cart.controller');
+/**
+ * @swagger
+ * tags:
+ *   - name: Order
+ *     description: API endpoints for order management
+ */
 const OrderController = require('./order.controller');
+/**
+ * @swagger
+ * tags:
+ *   - name: Review
+ *     description: API endpoints for review management
+ */
 const ReviewController = require('./review.controller');
 /**
  * @swagger
