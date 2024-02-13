271bd627040f67f8ef46f142fb9301f41adfaf3b
Pathum312 <pathum.2019557@iit.ac.lk>
2024-02-13 14:58:56 +0530

    Finished Swagger Doc

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -15,18 +15,118 @@ const orderService = new OrderService({
 	logger,
 });
 
-router.get('/', async (req, res, next) => {
-	await orderService.get(req, res, next);
-});
-
+/**
+ * @swagger
+ * /orders:
+ *   post:
+ *     summary: Add cart to an order
+ *     tags: [Order]
+ *     security:
+ *       - JWTAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: string
+ *             required:
+ *               - user_id
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.post('/', async (req, res, next) => {
 	await orderService.create(req, res, next);
 });
 
+/**
+ * @swagger
+ * /orders:
+ *   get:
+ *     summary: Get order listings
+ *     tags: [Order]
+ *     security:
+ *       - JWTAuth: []
+ *     parameters: [
+ *      {
+ *          name: 'user_id',
+ *          in: 'query',
+ *          description: '65dsfgfdsg'
+ *      },
+ *      {
+ *          name: 'status',
+ *          in: 'query',
+ *          description: 'PENDING'
+ *      },
+ *     ]
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
+router.get('/', async (req, res, next) => {
+	await orderService.get(req, res, next);
+});
+
+/**
+ * @swagger
+ * /orders:
+ *   put:
+ *     summary: Update order status
+ *     tags: [Order]
+ *     security:
+ *       - JWTAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               status:
+ *                 type: string
+ *             required:
+ *               - id
+ *               - status
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.put('/', async (req, res, next) => {
 	await orderService.update(req, res, next);
 });
 
+/**
+ * @swagger
+ * /orders/{id}:
+ *   delete:
+ *     summary: Delete order by id
+ *     tags: [Order]
+ *     security:
+ *       - JWTAuth: []
+ *     parameters: [
+ *      {
+ *          name: 'id',
+ *          in: 'path',
+ *          description: '65dsfsfbfh5634'
+ *      },
+ *     ]
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.delete('/:id', async (req, res, next) => {
 	await orderService.destroy(req, res, next);
 });
--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -12,14 +12,100 @@ const reviewService = new ReviewService({
 	logger,
 });
 
+/**
+ * @swagger
+ * /reviews:
+ *   post:
+ *     summary: Add a review for an product
+ *     tags: [Review]
+ *     security:
+ *       - JWTAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: string
+ *               product_id:
+ *                 type: string
+ *               rating:
+ *                 type: integer
+ *               comment:
+ *                 type: string
+ *             required:
+ *               - user_id
+ *               - product_id
+ *               - rating
+ *               - comment
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.post('/', async (req, res, next) => {
 	await reviewService.create(req, res, next);
 });
 
+/**
+ * @swagger
+ * /reviews:
+ *   put:
+ *     summary: Edit review
+ *     tags: [Review]
+ *     security:
+ *       - JWTAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               rating:
+ *                 type: integer
+ *               comment:
+ *                 type: string
+ *             required:
+ *               - id
+ *               - rating
+ *               - comment
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.put('/', async (req, res, next) => {
 	await reviewService.update(req, res, next);
 });
 
+/**
+ * @swagger
+ * /reviews/{id}:
+ *   delete:
+ *     summary: Delete review by id
+ *     tags: [Review]
+ *     security:
+ *       - JWTAuth: []
+ *     parameters: [
+ *      {
+ *          name: 'id',
+ *          in: 'path',
+ *          description: '65dsfsfbfh5634'
+ *      },
+ *     ]
+ *     responses:
+ *       '200':
+ *         description: Success response.
+ *       '500':
+ *         description: Internal server error.
+ */
 router.delete('/:id', async (req, res, next) => {
 	await reviewService.destroy(req, res, next);
 });
