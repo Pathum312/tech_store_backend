b2249272885873dae4ea410dff8d55a8512321fb
Pathum312 <pathum.2019557@iit.ac.lk>
2024-04-08 17:44:07 +0530

    Increased request limit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const { swaggerUI, specs } = require('./config');
 const { handleErrors } = require('./middleware/log.middleware');
 const { authenticateUser } = require('./middleware/auth.middleware');
@@ -15,8 +16,8 @@ const {
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Swagger Documentation
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 // API Routes
